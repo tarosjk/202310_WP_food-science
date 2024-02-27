f64b3833a102d3914c601380988032eab69c0977
tarosjk <tarosjk@gmail.com>
2024-02-27 11:00:30 +0900

    アーカイブページタイトル（年）を変更

--- wp-content/themes/food-science/index.php
+++ wp-content/themes/food-science/index.php
@@ -5,7 +5,7 @@
   <section class="section">
     <div class="section_inner">
       <div class="section_header">
-        <h1 class="heading heading-primary"><span>最新情報</span>NEWS - <?php wp_title(''); ?></h1>
+        <h1 class="heading heading-primary"><span>最新情報</span>NEWS - <?php wp_title(''); ?><?php if (is_year()) : ?>年<?php endif; ?></h1>
       </div>
 
       <div class="archive">
@@ -24,8 +24,12 @@
         <div class="archive_yealy">
           <h2 class="archive_title">年別</h2>
           <ul class="archive_list">
-            <li><a href='#'>2023</a></li>
-            <li><a href='#'>2022</a></li>
+            <?php
+            $args = [
+              'type' => 'yearly',
+            ];
+            wp_get_archives($args);
+            ?>
           </ul>
         </div>
       </div>
