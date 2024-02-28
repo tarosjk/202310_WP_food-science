fd54d3c47f0e1a78312d96354936bf978f7e7fad
tarosjk <tarosjk@gmail.com>
2024-02-28 14:45:06 +0900

    投稿詳細ページにてサブクエリを定義

--- wp-content/themes/food-science/single.php
+++ wp-content/themes/food-science/single.php
@@ -61,6 +61,31 @@
           </article>
       <?php endwhile;
       endif; ?>
+
+      <?php
+      $args = [
+        'post_type' => 'post', //投稿のみ
+        'posts_per_page' => 3,
+        'post__not_in' => [get_the_ID()],
+      ];
+      $latest_query = new WP_Query($args);
+
+      if ($latest_query->have_posts()) :
+      ?>
+        <section class="latest">
+          <header class="latest_header">
+            <h2 class="heading heading-secondary">新着情報</h2>
+          </header>
+          <div class="latest_body">
+            <div class="cardList">
+              <?php while ($latest_query->have_posts()) : $latest_query->the_post(); ?>
+                <?php get_template_part('template-parts/loop', 'news'); ?>
+              <?php endwhile; ?>
+            </div>
+          </div>
+        </section>
+      <?php endif; ?>
+
     </div>
   </div>
 </main>
