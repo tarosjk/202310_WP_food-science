4a150365d71276804ff5a4ff704fafc388c4f482
tarosjk <tarosjk@gmail.com>
2024-03-05 14:42:42 +0900

    カスタムブロックの作成（Lazy Blocksプラグイン）

---
+++ wp-content/themes/food-science/blocks/lazyblock-food-box/block.php
@@ -0,0 +1,18 @@
+<div class="foodCard foodCard-border">
+  <a href="<?= $attributes['url']; ?>">
+    <div class="foodCard_pic">
+      <?php
+      $pic = $attributes['pic'];
+      if (!empty($pic)) :
+      ?>
+        <img src="<?= $pic['url']; ?>" alt="">
+      <?php else : ?>
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/common/noimage.png" alt="">
+      <?php endif; ?>
+    </div>
+    <div class="foodCard_body">
+      <h4 class="foodCard_title"><?= $attributes['name']; ?></h4>
+      <p class="foodCard_price"><?= $attributes['price']; ?>円</p>
+    </div>
+  </a>
+</div>
